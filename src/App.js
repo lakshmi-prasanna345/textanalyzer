ea1572af874a8eaffafb6c3a8c40ea3a4976710a
Naveen sai Gavara <naveensai1729@gmail.com>
2024-01-27 22:07:53 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import TextAnalyzer from './components/TextAnalyzer';
+
+function App() {
+  return (
+    <div className="App">
+      <TextAnalyzer></TextAnalyzer>
+    </div>
+  );
+}
+
+export default App;
